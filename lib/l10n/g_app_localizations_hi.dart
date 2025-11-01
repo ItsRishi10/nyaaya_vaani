a71b9eda838e430dd58df3ac230068b056128f6b
ItsRishi10 <mrish234@gmail.com>
2025-11-01 16:17:13 +0530

    chore: update app name in platform manifests

---
+++ lib/l10n/g_app_localizations_hi.dart
@@ -0,0 +1,97 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'g_app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Hindi (`hi`).
+class GAppLocalizationsHi extends GAppLocalizations {
+  GAppLocalizationsHi([String locale = 'hi']) : super(locale);
+
+  @override
+  String get app_title => 'न्याय वाणी';
+
+  @override
+  String get legal_services => 'कानूनी सेवाएँ';
+
+  @override
+  String get nyaaya_whistle => 'न्याय व्हिसल';
+
+  @override
+  String get statistics => 'आंकड़े और रणनीति';
+
+  @override
+  String get youth => 'युवा संघ';
+
+  @override
+  String get legal_library => 'कानूनी पुस्तकालय';
+
+  @override
+  String get upload_image => 'तस्विर अपलोड करें';
+
+  @override
+  String get available_advocates => 'उपलब्ध वकील';
+
+  @override
+  String get request_help => 'मदद का अनुरोध करें';
+
+  @override
+  String get complaint_details => 'शिकायत विवरण';
+
+  @override
+  String get selected_location => 'चयनित स्थान';
+
+  @override
+  String get location_reporting =>
+      'स्थान रिपोर्टिंग (स्थान चिह्नित करने के लिए मानचित्र पर टैप करें)';
+
+  @override
+  String get submit_complaint => 'शिकायत दर्ज करें';
+
+  @override
+  String get complaint_submit => 'शिकायत प्रस्तुत करें';
+
+  @override
+  String get poll_results => 'मतदान परिणाम';
+
+  @override
+  String get sentiment_analysis => 'भाव विश्लेषण';
+
+  @override
+  String get upcoming_events => 'आगामी कार्यक्रम';
+
+  @override
+  String get gamification => 'खेल तत्व';
+
+  @override
+  String get ai_assistant => 'एआई सहायक';
+
+  @override
+  String get ai_prediction =>
+      'एआई भविष्यवाणी: जनता की राय समर्थन की ओर स्थानांतरित होने की संभावना है।';
+
+  @override
+  String get oppose => 'विरोध';
+
+  @override
+  String get support => 'सहायता';
+
+  @override
+  String get join => 'जोड़ना';
+
+  @override
+  String get points => 'अंक';
+
+  @override
+  String get badge => 'बैज: सिविक स्टार, स्वयंसेवक नायक';
+
+  @override
+  String get library_text =>
+      'जागरूकता और सीखने के लिए प्रमुख भारतीय कानूनी संसाधनों का एक सरलीकृत भंडार';
+
+  @override
+  String get library_search => 'कानूनी संसाधन खोजें';
+
+  @override
+  String get no_result => 'कोई परिणाम नहीं मिला';
+}
